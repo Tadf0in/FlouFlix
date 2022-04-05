4b95a7597ff2b63e057d6fc91c292e3638498c8b
AMOUDRUZ Louis <louis.amoudruz@gmail.com>
2022-04-05 15:53:20 +0200

    fin de l'heure

--- Latest/browse/classes/files.py
+++ Latest/browse/classes/files.py
@@ -6,21 +6,36 @@ from browse.models import Serie as Serie_DB
 
 
 def get_movies_dir():
+    movies = []
     moviesdir = os.listdir('browse/static/films')
     for moviedir in moviesdir:
         id = int(moviedir.split('.')[0])
         movie = Movie(id)
         movie.to_db('owned')
+        movies.append(movie)
+    return movies
 
 
 def get_series_dir():
+    series = []
     seriesdir = os.listdir('browse/static/series')
     for seriedir in seriesdir:
         id = int(seriedir.split('.')[0])
         serie = Serie(id)
         serie.to_db('owned')
+        series.append(serie)
+    return series 
 
 
 def get_total_dir():
-    get_movies_dir()
-    get_series_dir()
+    movies_db = Movie_DB.objects.filter(status='owned')
+    movies_id = [movie.id for movie in movies_db]
+    movies_dir = [movie.id for movie in get_movies_dir()]
+
+    for movie in movies_id:
+        if movie not in movies_dir:
+            Movie(movie).to_db('clicked')
+        
+
+    series_db = Serie_DB.objects.filter(status='owned')
+    series_id = [serie.id for serie in series_db]
